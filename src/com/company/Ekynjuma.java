5c9bca434ded165317d9fb046e9957ff9986619a
Jamal <jamalpeaksoft@gmail.com>
2022-02-27 21:31:50 +0600

    Initial commit

---
+++ src/com/company/Ekynjuma.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public enum Ekynjuma {
+    DYISCHOMBY,SCHARSCHEMBI,SCHEISCHEMBI,BEISCHEMBI,JUMA,ISCHEMBI,JECSCHEMBI
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+       Ekynjuma kyn1 = Ekynjuma.DYISCHOMBY;
+        Ekynjuma kyn2 = Ekynjuma.SCHEISCHEMBI;
+        Ekynjuma kyn3 = Ekynjuma.SCHARSCHEMBI;
+        Ekynjuma kyn4 = Ekynjuma.BEISCHEMBI;
+        Ekynjuma kyn5 = Ekynjuma.JUMA;
+        Ekynjuma kyn6 = Ekynjuma.ISCHEMBI;
+        Ekynjuma kyn7 = Ekynjuma.JECSCHEMBI;
+
+     switch (kyn1) {
+         case DYISCHOMBY:
+             System.out.println(" Дуйшоьбу  куну  JAVAнын   теориясы боюнча  лекция  болот");
+
+         }
+switch (kyn2) {
+    case SCHEISCHEMBI  :
+        System.out.println(" Шейшемби куну  JAVAнын   практикасы   болот");
+
+        break;}
+
+
+        switch (kyn3) {
+        case SCHARSCHEMBI:
+        System.out.println(" Шаршемби куну  JAVAнын   теориясы  жана  SOFTSCILS боюнча сабак болот");
+
+        break;}
+
+        switch (kyn4) {
+            case BEISCHEMBI :
+        System.out.println(" Бейшемби  куну  JAVAнын  практикасы жана  англис  тили  болот");
+
+        break;}
+
+
+        switch (kyn5) {
+            case JUMA :
+        System.out.println(" Жума  куну  JAVAнын   теориясы боюнча  лекция  болот");
+
+        break;}
+
+        switch (kyn6) {
+            case ISCHEMBI :
+        System.out.println(" Ишемби  куну  уй жыйнайм.неберелер  келишет аларды  карайм");
+
+        break;}
+
+
+        switch (kyn7) {
+            case JECSCHEMBI :
+        System.out.println(" Жекшемби  куну  JAVAны карайм.конокко барам  же  алар келишет");
+
+        break;}
+
+       }}
